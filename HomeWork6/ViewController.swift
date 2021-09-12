28f2b99f04d1f212ad1f010962ddf7406086ac65
MikeKoshelev <novygeroy@yandex.ru>
2021-09-12 18:10:50 +0700

    Initial Commit

---
+++ HomeWork6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork6
+//
+//  Created by Михаил Кошелев on 12.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
